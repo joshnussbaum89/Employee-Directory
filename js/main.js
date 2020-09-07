4b322f8799dcf1dfd702cb302c15347050387c34
Josh Nussbaum <Joshnussbaum89@gmail.com>
2020-09-07 12:27:34 -0400

    test layout with no JS

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 /* ============================================= */
 /*              JavaScript                       */
 /* ============================================= */
-fetch('https://randomuser.me/api/')
+fetch('https://randomuser.me/api/?results=12')
     .then(response => response.json())
     .then(data => console.log(data.results)); // create function to print results to page
